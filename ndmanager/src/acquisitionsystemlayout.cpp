119853e92d28599cc75969b959c96f7f56d99bc0
Montel Laurent <montel@kde.org>
2012-08-28 15:24:47 +0200

    Port to uic4

---
+++ ndmanager/src/acquisitionsystemlayout.cpp
@@ -0,0 +1,9 @@
+#include "acquisitionsystemlayout.h"
+
+AcquisitionSystemLayout::AcquisitionSystemLayout( QWidget* parent )
+    : QWidget( parent ), Ui_AcquisitionSystemLayout()
+{
+    setupUi( this );
+
+}
+#include "acquisitionsystemlayout.moc"
---
+++ ndmanager/src/acquisitionsystemlayout.h
@@ -0,0 +1,22 @@
+#ifndef ACQUISITIONSYSTEMLAYOUT_H
+#define ACQUISITIONSYSTEMLAYOUT_H
+
+#include "ui_acquisitionsystemlayout.h"
+
+
+class AcquisitionSystemLayout : public QWidget, private Ui_AcquisitionSystemLayout
+{
+    Q_OBJECT
+
+public:
+    AcquisitionSystemLayout( QWidget* parent = 0 );
+    
+
+public slots:
+   // TODO copy 0 functions, 0 signals, and 0 slots (not init and destroy!) from acquisitionsystemlayout.h.old here
+
+private:
+   
+};
+
+#endif
